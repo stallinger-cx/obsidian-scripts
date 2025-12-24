ca9a86877cd4ab0b109ac39f8e59c087be89fcfa
Michael J. Stallinger <mj@stallinger.dev>
2025-12-24 01:32:32 +0100

    Add basic vscode configuration

---
+++ .vscode/globals.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Global definitions for Obsidian and Dataview
+ */
+
+// Dataview API
+declare const dv: any;
+// Moment.js (always available in Obsidian)
+declare const moment: any;
+// CustomJS Container
+declare const customJS: {
+    [key: string]: any;
+};
+// Obsidian app object (if necessary)
+declare const app: any;
