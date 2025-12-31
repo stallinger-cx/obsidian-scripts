c913a2d40b52e7ae18addc1a1ac74f670197f8fb
Michael J. Stallinger <mj@stallinger.dev>
2025-12-31 08:13:43 +0100

    Fix types DataviewApi to DataviewInlineApi

--- globals.d.ts
+++ globals.d.ts
@@ -7,13 +7,13 @@
 
 // Imports (aliased!!)
 //  Redefining the internal name, to get the correct names globally!
-import { DataviewApi as _DataviewApi } from "obsidian-dataview";
+import { DataviewInlineApi as _DataviewInlineApi } from "obsidian-dataview";
 import * as _moment from "moment";
 
 declare global {
 
     // JSDoc for globally visible types
-    type DataviewApi = _DataviewApi;
+    type DataviewInlineApi = _DataviewInlineApi;
     const moment: typeof _moment; // Trick: merge function with namespace
 
     // todo: my tablecolumn-Def ... to be removed
@@ -27,7 +27,7 @@ declare global {
 
     // global variables
     const customJS: any;
-    const dv: DataviewApi;
+    const dv: DataviewInlineApi;
     const input: any;
 //    const app: any;
 }
