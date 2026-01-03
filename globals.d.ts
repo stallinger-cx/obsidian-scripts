b596140a5881aa1045a083e5a118df646647d28a
Michael J. Stallinger <mj@stallinger.dev>
2026-01-03 20:25:58 +0100

    Refactor data-classes to become DataView-Agnostic - Add paragraph() for ViewRenderer; - Abstact DataviewInlineApi and use interface segregation; - Move DataViewServiceContainer to lib-root; - Enhance DataViewScriptRunner; - Fix typedefs and JSDoc;

--- globals.d.ts
+++ globals.d.ts
@@ -10,11 +10,33 @@
 import { DataviewInlineApi as _DataviewInlineApi } from "obsidian-dataview";
 import * as _moment from "moment";
 
+/**
+ * DataLoader API interface (part of DataviewInlineApi)
+ * @see .dev\node_modules\obsidian-dataview\lib\api\inline-api.d.ts for details
+ */
+interface _DataLoaderApi {
+    pages(query?: string): DataArray<any>;
+}
+
+/**
+ * DataRenderer API interface (part of DataviewInlineApi)
+ * @see .dev\node_modules\obsidian-dataview\lib\api\inline-api.d.ts for details
+ */
+interface _DataRendererApi {
+    header(level: number, text: any, options?: DomElementInfo): HTMLHeadingElement;
+    paragraph(text: any, options?: DomElementInfo): HTMLParagraphElement;
+    span(text: any, options?: DomElementInfo): HTMLSpanElement;
+    view(viewName: string, input: any): Promise<void>;
+    list(values?: any[] | DataArray<any>): Promise<void>;
+    table(headers: string[], values?: any[][] | DataArray<any>): Promise<void>;
+}
+
 declare global {
 
     // JSDoc for globally visible types
-    type DataviewInlineApi = _DataviewInlineApi;
-    const moment: typeof _moment; // Trick: merge function with namespace
+    type DataviewInlineApiInstance = _DataviewInlineApi;
+    type DataLoaderApi = _DataLoaderApi;
+    type DataRendererApi = _DataRendererApi;
 
     // todo: my tablecolumn-Def ... to be removed
     interface TableColumns {
@@ -27,7 +49,8 @@ declare global {
 
     // global variables
     const customJS: any;
-    const dv: DataviewInlineApi;
+    const dv: DataviewInlineApiInstance;
+    const moment: typeof _moment; // Trick: merge function with namespace
     const input: any;
 //    const app: any;
 }
